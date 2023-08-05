6c30c1351b832b514563895652ca839e4b4c9a65
deathbydogtreat <secretline31@hotmail.com>
2023-08-04 22:24:56 -0500

    Updates and steps for the next phase

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,20 +2,56 @@
 
 // See https://aka.ms/new-console-template for more information
 
-//To open DAT files
+// To open DAT files, 8.03
 // 1. Read in the file into memory
 //  1.A Read into Byte Array specifically
 // 2. Convert Byte Array into a class
 //  2.A Convert raw bytes into ASCII
 // 3. Diplay class data to the console
 // 4. Create workable way to search for specific information
-
-// First thing I should do is take very first template and print out something with it
 // Start with just trying to get name - https://github.com/engineerjames/men-among-gods/blob/main/src/common/Character.h
 
-// 1st successful name is "1" "Blank/0, 2nd successful name is "BlankTemplate/0""
+// New set of instructions, 8.04
+// 1. Proper Input Handling, i.e.- what happens if user types in 14 on main menu?
+// 2. When user inputs character number, what happens if that is NOT a number, like F or pizzahut?
+// 3. Last front end modification, keep application going unless end user truly wants to exit. (think while loop for something this simple)
+// 4. Nice to print out indication if template is used or not. A console.writeline advising "This template is Used." or "This template is not Used."
+// 5. Eliminate junk data, whenever a template is printed (Blank Template also has a bunch of shit at the end, how do you get rid of that?) HINT- null termination byte value is 0, look for first 0 then stop
+// 6. Bonus- Pull out the kindred value (templar, seyan, etc.) starts on byte 285. Is a 4 byte integer. 
+// 7. Bonus Bonus- To identify sex AND class, Ishtar would combine bit level items. Take 4 bytes, stuff into integer, 
 
-//getstring(byte[]) will play a part in this
+
+
+
+//static const constexpr unsigned int KIN_MERCENARY   = ( 1u << 0 ); 0000 0000 0000 0001 = 1
+//static const constexpr unsigned int KIN_SEYAN_DU    = ( 1u << 1 ); 0000 0000 0000 0010 = 2
+//static const constexpr unsigned int KIN_PURPLE      = ( 1u << 2 ); 0000 0000 0000 0100 = 4
+//static const constexpr unsigned int KIN_MONSTER     = ( 1u << 3 );
+//static const constexpr unsigned int KIN_TEMPLAR     = ( 1u << 4 );
+//static const constexpr unsigned int KIN_ARCHTEMPLAR = ( 1u << 5 );
+//static const constexpr unsigned int KIN_HARAKIM     = ( 1u << 6 );
+//static const constexpr unsigned int KIN_MALE        = ( 1u << 7 );
+//static const constexpr unsigned int KIN_FEMALE      = ( 1u << 8 );
+//static const constexpr unsigned int KIN_ARCHHARAKIM = ( 1u << 9 );
+//static const constexpr unsigned int KIN_WARRIOR     = ( 1u << 10 );
+//static const constexpr unsigned int KIN_SORCERER    = ( 1u << 11 );
+//
+//
+// 1. Get integer value of kindred - myKindred
+// 2. if (myKindred & KIN_MERCENARY) { I'm a mercenary! }
+// 3. if (myKindred & KIN_MERCENARY && myKindred & KIN_FEMALE) { I'm a female merc! }
+//
+//
+// myKindred   =   0000 0000 0000 0001
+// KIN_MERCENARY & 0000 0000 0000 0001
+//                 -------------------
+//                 0000 0000 0000 0001 != 0  (TRUE!)
+ 
+// myKindred   =   0000 0000 0000 0001
+// KIN_SEYAN     & 0000 0000 0000 0010
+//                 -------------------
+//                 0000 0000 0000 0000 == 0   (FALSE!)
+
 
 static string MainMenu()
 {
@@ -33,14 +69,15 @@ static string MainMenu()
 static string ReRunData()
 {
     Console.Clear();
-    Console.WriteLine("Input your starting byte and press Enter.");// Makes this modular, can now ask inside the program instead of doing it beforehand.
-    int startingbyte = (Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine("Input your desired character number and press Enter.");
+    // Things to keep in mind-
+    //  Need to find starting byte
+    //  Size of character 3605 bytes
+    int startingbyte = (Convert.ToInt32(Console.ReadLine())) * 3605 + 1;
 
-    Console.WriteLine("Now input the number of bytes you want to read, and press Enter.");// Makes this modular, can now ask inside the program instead of doing it beforehand.
-    int startingbytestoread = (Convert.ToInt32(Console.ReadLine()));
-
-    int startByteOffset = startingbyte; // Adjust the starting byte offset
-    int bytesToRead = startingbytestoread; // Adjust the number of bytes you want to read
+    // Since we are looking strictly for names, this value can remain static.
+    int startingbytestoread = 40; 
+    
 
     string filePath = @"C:\Users\secre\tchar.dat";
 
@@ -49,12 +86,12 @@ static string ReRunData()
         try
         {
             byte[] fileContent = File.ReadAllBytes(filePath);
-            int contentLength = Math.Min(bytesToRead, fileContent.Length - startByteOffset);
+            int contentLength = Math.Min(startingbytestoread, fileContent.Length - startingbyte);
             byte[] buffer = new byte[contentLength];
-            Array.Copy(fileContent, startByteOffset, buffer, 0, contentLength);
+            Array.Copy(fileContent, startingbyte, buffer, 0, contentLength);
 
             string content = Encoding.ASCII.GetString(buffer);
-            Console.WriteLine("Read content from byte offset " + startByteOffset + ":");
+            Console.WriteLine("Read content from byte offset " + startingbyte + ":");
             Console.WriteLine(content);
         }
         catch (Exception ex)
@@ -67,6 +104,7 @@ static string ReRunData()
         Console.WriteLine("File does NOT exist.");
     }
 
+    Console.WriteLine("");
     Console.WriteLine("Would you like to run a different set of numbers?");
     Console.WriteLine("1: Yes, I want to run a different set.");
     Console.WriteLine("2: No, I want to exit");
